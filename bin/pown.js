712ac3e27e507bb0e4e097168f36731da2c57069
Petko D. Petkov <pdp@gnucitizen.org>
2019-02-04 09:18:22 +0000

    Fixed loading if preferences directory does not exists.

--- bin/pown.js
+++ bin/pown.js
@@ -1,24 +1,30 @@
 #!/usr/bin/env node
 
-const path = require('path')
-const { getPreferencesDirectory } = require('@pown/preferences')
+const boot = async() => {
+    const path = require('path')
+    const { ensurePreferencesFilename, getPreferencesDirectory } = require('@pown/preferences')
 
-// stage1: setup pown root
+    // stage1: setup pown root
 
-if (!process.env.POWN_ROOT) {
-    process.env.POWN_ROOT = path.join(__dirname, '..')
+    if (!process.env.POWN_ROOT) {
+        process.env.POWN_ROOT = path.join(__dirname, '..')
+    }
+
+    // stage2: setup node modules
+
+    await ensurePreferencesFilename('modules', 'package.json')
+
+    const dirname = getPreferencesDirectory('modules')
+
+    module.paths.push(dirname)
+
+    // stage3: setup pown modules
+
+    process.env.POWN_PATH = [...(process.env.POWN_PATH ? process.env.POWN_PATH : []), dirname].join(path.delimiter)
+
+    // stage4: launch
+
+    require('@pown/cli/bin/cli')
 }
 
-// stage2: setup node modules
-
-const dirname = getPreferencesDirectory('modules')
-
-module.paths.push(dirname)
-
-// stage3: setup pown modules
-
-process.env.POWN_PATH = [...(process.env.POWN_PATH ? process.env.POWN_PATH : []), dirname].join(path.delimiter)
-
-// stage4: launch
-
-require('@pown/cli/bin/cli')
+boot()
