cfa6004674ff3be3fe5a97eb61631c2dd0054b10
Petko D. Petkov <pdp@gnucitizen.org>
2020-08-28 14:53:55 +0000

    Simplified main script.

--- bin/pown.js
+++ bin/pown.js
@@ -4,29 +4,19 @@ const boot = async() => {
     const path = require('path')
     const { ensurePreferencesFilename, getPreferencesDirectory } = require('@pown/preferences')
 
-    // stage0: add the local node_modules to the module search path
-    // NOTE: this is mute
-
-    module.paths.push(path.join(__dirname, '..', 'node_modules'))
-
     // stage1: setup pown root
 
     if (!process.env.POWN_ROOT) {
         process.env.POWN_ROOT = path.join(__dirname, '..')
     }
 
-    // stage2: add modules node_modules to the module search path
-    // NOTE: this is mute
+    // stage2: setup pown modules
 
     await ensurePreferencesFilename('modules', 'package.json')
 
     const dirname = getPreferencesDirectory('modules')
 
-    module.paths.push(dirname)
-
-    // stage3: setup pown modules
-
-    process.env.POWN_PATH = [...(process.env.POWN_PATH ? [process.env.POWN_PATH] : []), dirname].join(path.delimiter)
+    process.env.POWN_PATH = [dirname, ...(process.env.POWN_PATH ? [process.env.POWN_PATH] : [])].join(path.delimiter)
 
     // stage4: launch
 
