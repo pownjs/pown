4e0018e48fd3b44b54e618baa949f1f3f026e2c2
Petko D. Petkov <pdp@websecurify.com>
2022-10-28 21:57:23 +0000

    Improved scheduler with builtin bar and everything.

--- packages/request/lib/scheduler.js
+++ packages/request/lib/scheduler.js
@@ -3,6 +3,19 @@ const Bottleneck = require('bottleneck')
 
 const { request } = require('./request')
 
+const BARRED_ERROR_CODE = 'BARRED'
+
+class BarredError extends Error {
+  constructor(message) {
+    super(message)
+
+    this.code = BARRED_ERROR_CODE
+  }
+}
+
+/**
+ * The system limiter is a global throttle for all requests.
+ */
 const systemLimiter = new Bottleneck()
 
 /**
@@ -16,10 +29,14 @@ class SystemScheduler extends EventEmitter {
 
     this.limiter = systemLimiter
 
-    const { maxRetries = 5, retryBackoff = 1000 } = options || {}
+    const { maxRetries = 5, retryBackoff = 1000, maxFailuresToBar = 10 } = options || {}
 
     this.maxRetries = maxRetries
     this.retryBackoff = retryBackoff
+
+    this.maxFailuresToBar = maxFailuresToBar
+
+    this.barredOrigins = {}
   }
 
   update(options) {
@@ -38,6 +55,34 @@ class SystemScheduler extends EventEmitter {
     this.update({ reservoir: null })
   }
 
+  getBarOrigin = (request) => {
+      return request.uri?.replace(/^(https?:\/\/[^/]+).*/i, '$1') || ''
+  }
+
+  reverseBar = (request) => {
+      const origin = this.getBarOrigin(request)
+
+      delete this.barredOrigins[origin]
+  }
+
+  riseBar = (request) => {
+      const origin = this.getBarOrigin(request)
+
+      this.barredOrigins[origin] = (this.barredOrigins[origin] || 0) + 1
+  }
+
+  permanentlyBar(request) {
+      const origin = this.getBarOrigin(request)
+
+      this.barredOrigins[origin] = this.maxFailuresToBar
+  }
+
+  isBarred = (request) => {
+      const origin = this.getBarOrigin(request)
+
+      return this.barredOrigins[origin] && this.barredOrigins[origin] >= this.maxFailuresToBar
+  }
+
   async request(options) {
     this.emit('request-scheduled', options)
 
@@ -49,19 +94,31 @@ class SystemScheduler extends EventEmitter {
       let retries = 0
 
       for (;;) {
+        if (this.isBarred(options)) {
+          result = { ...options, info: { error: new BarredError('Barred') } }
+
+          break
+        }
+
         result = await request(options)
 
         if (result.info?.error) {
           if (result.info.error.code === 'ENOTFOUND') {
+            this.permanentlyBar(options)
+
             break
           }
           else {
+            this.riseBar(options)
+
             await new Promise((resolve) => {
               setTimeout(resolve, this.retryBackoff * Math.pow(2, retries++))
             })
           }
         }
         else {
+          this.reverseBar(options)
+
           break
         }
       }
@@ -110,6 +167,10 @@ class Scheduler extends SystemScheduler {
 }
 
 module.exports = {
+  BARRED_ERROR_CODE,
+
+  BarredError,
+
   systemLimiter,
 
   SystemScheduler,
