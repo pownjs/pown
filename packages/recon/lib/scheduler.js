abaa85c5a9c6b07d61847025ebcdce7da24de3dd
Petko D. Petkov <pdp@websecurify.com>
2022-10-28 23:17:23 +0000

    Simplified scheduler.

--- packages/recon/lib/scheduler.js
+++ packages/recon/lib/scheduler.js
@@ -9,44 +9,23 @@ class Scheduler extends PownScheduler {
     if (request.headers && !request.headers['user-agent']) {
       request = {
         ...request,
+
         headers: { ...request.headers, 'user-agent': 'pown' },
       }
     }
 
-    const { maxRetries = 5, retryDely = 5000 } = request
+    const res = await this.request(request)
 
-    let res
-
-    for (let i = 0; i < maxRetries; i++) {
-      if (i > 0) {
-        await sleep(retryDely)
-      }
-
-      try {
-        res = await this.request(request)
-      } catch (e) {
-        if (process.env.NODE_ENV !== 'production') {
-          console.error(e)
-        }
-
-        continue
-      }
-
-      if (res.info.error) {
-        console.error(`${res.method || 'GET'}`, res.uri, '->', res.info.error)
-      } else {
-        console.debug(
-          `${res.method || 'GET'}`,
-          res.uri,
-          '->',
-          res.responseCode,
-          Buffer.from(res.responseBody).slice(0, 512).toString('base64')
-        )
-
-        if (res.responseCode < 500) {
-          break
-        }
-      }
+    if (res.info.error) {
+      console.error(`${res.method || 'GET'}`, res.uri, '->', res.info.error)
+    } else {
+      console.debug(
+        `${res.method || 'GET'}`,
+        res.uri,
+        '->',
+        res.responseCode,
+        Buffer.from(res.responseBody).slice(0, 512).toString('base64')
+      )
     }
 
     if (!res || res.responseCode >= 500) {
